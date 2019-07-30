6450bab7dea43e2d75d7fa39d4a4454008cdafbe
Andre <andre.webderking@gmail.com>
2019-07-30 14:38:24 -0300

    Set webpack-server environment

--- es6/exercises/module_01/06.js
+++
@@ -1,4 +0,0 @@
-const user = "Diego";
-const age = 23;
-
-console.log(`User ${user} has ${age} years old.`);
--- es6/exercises/module_01/07.js
+++
@@ -1,9 +0,0 @@
-const name = 'Diego';
-const age = 23;
-const user = {
- name,
- age,
- city: 'Rio do Sul',
-};
-
-console.log(user);
