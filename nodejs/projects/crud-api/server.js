b8cb645ae9f4dbc949c7c19361cad31e151b727d
Andre <andre.webderking@gmail.com>
2019-08-01 11:38:02 -0300

    Crate ProductController

--- nodejs/projects/crud-api/server.js
+++ nodejs/projects/crud-api/server.js
@@ -8,17 +8,6 @@ mongoose.connect('mongodb://localhost:27017/crud-api', {useNewUrlParser: true});
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-app.get('/', (req, res) => {
-
-    Product.create({
-        title: "AngularJS",
-        description: "Build interactive javascript applications with AngularJS",
-        url: 'https://github.com/angular/angular.js/'
-    })
-
-    return res.send('Hello Rocketseat!');
-})
+app.use('/api', require('./src/routes'))
 
 app.listen(3001);
