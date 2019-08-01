ba8bb146f1ca546125435d61c13599b9467f052f
Andre <andre.webderking@gmail.com>
2019-08-01 14:50:37 -0300

    Create CRUD for Product model

--- nodejs/projects/crud-api/src/controllers/ProductController.js
+++ nodejs/projects/crud-api/src/controllers/ProductController.js
@@ -2,8 +2,49 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports= {
+
     async index(req, res) {
-        const products = await Product.find();
-        return res.json(products);
+        try {
+            const products = await Product.find();
+            return res.json(products);            
+        } catch (error) {
+            return res.send(error);
+        }
+    },
+    
+    async show(req, res) {
+        try {
+            const product = await Product.findById(req.params.id);
+            return res.json(product);
+        } catch (error) {
+            return res.send(error);
+        }
+    },
+
+    async store(req, res) {
+        try {
+            const product = await Product.create(req.body);
+            return res.json(product);
+        } catch (error) {
+            return res.send(error);
+        }
+    },
+
+    async update(req, res) {
+        try {
+            const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+            return res.json(product);
+        } catch (error) {
+            return res.send(error);
+        }
+    },
+
+    async destroy(req, res) {
+        try {
+            await Product.findByIdAndRemove(req.params.id);
+            return res.send();
+        } catch (error) {
+            return res.send(error);
+        }
     }
 }
