adf865557958c2d4138032f8bfe78e689d35c6c5
Andre <andre.webderking@gmail.com>
2019-08-01 19:11:30 -0300

    Create Header component

--- reactjs/projects/productize/src/App.js
+++ reactjs/projects/productize/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './styles.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Productize</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
---
+++ reactjs/projects/productize/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">
+        <h1>Productize</h1>
+    </header>
+);
+
+export default Header;
