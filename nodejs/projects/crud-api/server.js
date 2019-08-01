2ee40ed5d20c961a2f9e046543aad449d6fa4145
Andre <andre.webderking@gmail.com>
2019-08-01 15:37:02 -0300

    Add cors

--- nodejs/projects/crud-api/server.js
+++ nodejs/projects/crud-api/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/crud-api', {useNewUrlParser: true});
 
 requireDir('./src/models');
 
-app.use('/api', require('./src/routes'))
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
