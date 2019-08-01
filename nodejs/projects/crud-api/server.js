3bd19539cb6314912100e81bb2463add3e725e49
Andre <andre.webderking@gmail.com>
2019-08-01 10:52:59 -0300

    Create Product model/schema

--- nodejs/projects/crud-api/server.js
+++ nodejs/projects/crud-api/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/crud-api', {useNewUrlParser: true});
 
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat!');
+
+    Product.create({
+        title: "AngularJS",
+        description: "Build interactive javascript applications with AngularJS",
+        url: 'https://github.com/angular/angular.js/'
+    })
+
+    return res.send('Hello Rocketseat!');
 })
 
 app.listen(3001);
