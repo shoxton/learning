31980e9dff827bb2fd4b0fc354720b58ade0b925
Andre <andre.webderking@gmail.com>
2019-08-02 15:20:13 -0300

    Listing products from api

--- reactjs/projects/productize/src/pages/Main/index.js
+++ reactjs/projects/productize/src/pages/Main/index.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import './styles.css';
+import Productize from '../../services/api';
 
 class Main extends Component {
+
+    state = {
+        products: []
+    }
+
+    ProductService = new Productize();
+
+    componentDidMount() {
+        this.loadProducts();
+        console.log(this)
+    }
+
+    loadProducts = async () => {
+        const response = await this.ProductService.list();
+        this.setState({ products: response.data.docs })
+    }
+
     render() {
+
+        const { products } = this.state;
+
         return(
-            <main id="main">
-                <h1>Main</h1>
+            <main className="product-list">
+                {products.map( product => (
+                    <article key={product._id}>
+                        <h3>{product.title}</h3>
+                        <p>{product.description}</p>
+                        <a href="">More</a>
+                    </article>
+                ) )}
             </main>
         );
     }
--- reactjs/projects/productize/src/services/api.js
+++ reactjs/projects/productize/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({baseURL: 'http://localhost:3001/api'});
 
 class Productize {
 
-    async List() {
+    async list() {
         const response = await API.get('/products');
         return response;
     }
