8d1267369b6d4b2c6c5777835633bb76c7a30e02
Andre <andre.webderking@gmail.com>
2019-08-09 21:07:11 -0300

    Add real time match functionallity

--- reactjs/projects/tindev-frontend/src/pages/Main.js
+++ reactjs/projects/tindev-frontend/src/pages/Main.js
@@ -8,12 +8,21 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({match}) {
     const [ users, setUsers ] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
-        const socket = io('http://localhost:3333');
+        const socket = io('http://localhost:3333', {
+            query: {user: match.params.id}
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
+        
     }, [match.params.id])
 
     useEffect(() => {
@@ -74,6 +83,18 @@ export default function Main({match}) {
                     <strong>Acabou :(</strong>
                 </div>
             ) }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match"/>
+
+                    <img className="avatar" src={matchDev.avatar} alt=""/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    
+                    <button onClick={() => setMatchDev(null)} type="button">Fechar</button>
+                </div>
+            )}
 
         </div>   
     );
