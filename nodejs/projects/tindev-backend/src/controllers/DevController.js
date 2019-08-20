49695f426be150fe89a92a2c4e4b73c2480bb1fd
Andre <andre.webderking@gmail.com>
2019-08-20 10:07:51 -0300

    Update tindev api dev controller

--- nodejs/projects/tindev-backend/src/controllers/DevController.js
+++ nodejs/projects/tindev-backend/src/controllers/DevController.js
@@ -19,24 +19,29 @@ module.exports = {
     },
     async store(req, res) {
         const { username } = req.body;
-        
-        const userExists = await Dev.findOne({ user: username });
 
-        if (userExists) {
-            return res.json(userExists);
+        try {
+            const userExists = await Dev.findOne({ user: username });
+
+            if (userExists) {
+                return res.json(userExists);
+            }
+
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+
+            const { name, bio, avatar_url: avatar } = response.data;
+
+            const dev = await Dev.create({
+                user: username,
+                name,
+                bio,
+                avatar
+            })
+
+            return res.json(dev)
+
+        } catch (error) {
+            res.send(error)
         }
-
-        const response = await axios.get(`https://api.github.com/users/${username}`)
-
-        const { name, bio, avatar_url: avatar } = response.data;
-
-        const dev = await Dev.create({
-            user: username,
-            name,
-            bio,
-            avatar
-        })
-
-        return res.json(dev)
     }
 }
